1c3cc7401b183fa5d2143b315f1ddd46abf7d1e5
Allename <allenameanthony2000@gmail.com>
2021-02-14 01:58:47 +0100

    first commit

--- lib/widgets/bar_chart.dart
+++ lib/widgets/bar_chart.dart
@@ -18,13 +18,79 @@ class BarChart extends StatelessWidget {
         ),
         SizedBox(height: 5.0),
         Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
             IconButton(
-              icon: Icon(
-                Icons.arrow_back,
+              icon: Icon(Icons.arrow_back),
+              iconSize: 30.0,
+              onPressed: () {},
+            ),
+            Text(
+              'Nov 10, 2019 - Nov 16, 2019',
+              style: TextStyle(
+                fontSize: 15.0,
+                fontWeight: FontWeight.w600,
+                letterSpacing: 1.2,
               ),
             ),
+            IconButton(
+              icon: Icon(Icons.arrow_forward),
+              iconSize: 30.0,
+              onPressed: () {},
+            ),
           ],
+        ),
+        SizedBox(height: 30.0),
+        Row(
+          children: <Widget>[
+            Bar(
+              label: 'Su',
+              amountSpent: expenses[0],
+              mostExpensive: 200,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class Bar extends StatelessWidget {
+  final String label;
+  final double amountSpent;
+  final double mostExpensive;
+
+  final double _maxBarHeight = 150.0;
+
+  Bar({this.label, this.amountSpent, this.mostExpensive});
+
+  @override
+  Widget build(BuildContext context) {
+    final barHeight = amountSpent / mostExpensive * _maxBarHeight;
+    return Column(
+      children: <Widget>[
+        Text(
+          '\$${amountSpent.toStringAsFixed(2)}',
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        SizedBox(height: 6.0),
+        Container(
+          height: barHeight,
+          width: 18.0,
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+            borderRadius: BorderRadius.circular(6.0),
+          ),
+        ),
+        SizedBox(height: 8.0),
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.w600,
+          ),
         ),
       ],
     );
